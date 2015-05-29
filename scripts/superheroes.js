13a1f9fc9d22152a8a211dbba61f7db2d5e963bb
Jorge Epuñan <juanbrujo@gmail.com>
2015-05-29 14:26:59 -0400

    nuevo: entrega al azar el nombre de un super-heroe ficticio

---
+++ scripts/superheroes.js
@@ -0,0 +1,24 @@
+// Description:
+//  entrega al azar el nombre de un super-heroe ficticio
+//
+// Dependencies:
+//  superheroes
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   superheroe
+//
+// Author:
+//   jorgeepunan
+
+var superheroes = require("superheroes"); 
+
+module.exports = function(robot) {
+
+  robot.respond(/superheroe/i, function(res) {
+    res.send( "Súper-Héroe: " + superheroes.random() );
+  });
+
+};
