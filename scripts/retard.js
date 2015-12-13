b710bb57436a429d30807c80fabf40a36fef1b16
Adrián Salgado <devutwubs@gmail.com>
2015-12-13 01:21:58 -0300

    fuck it, back to the old one

--- scripts/retard.js
+++ scripts/retard.js
@@ -15,13 +15,10 @@
 
 module.exports = function(robot) {
   return robot.hear(/retard|retardao|:retard:|:retardao:/gi, function(msg) {
-    var reaction = msg.message.text.replace(/:|\s+/g, '');
-    console.log('reaction:', reaction);
-
     msg.http("https://slack.com/api/reactions.add")
     .query({
       token: process.env.HUBOT_SLACK_TOKEN,
-      name: reaction,
+      name: 'retard',
       channel: msg.message.rawMessage.channel,
       timestamp: msg.message.id
     });
