2e47c83f276ecf72c37464d9e437e9e093691739
Camilo Castro <camilo@jumpitt.com>
2016-01-03 00:36:27 -0300

    added 420 command

--- scripts/helpers/random.js
+++ scripts/helpers/random.js
@@ -1,7 +1,21 @@
-//Description:
+// Description:
+//  Random Item Helper
 //
+// Dependencies:
+//  None
+//
+// Configuration:
+//   None
+//
+// Usage:
+// var random = require('./helpers/random');
+// var item = random.item(items);
+//
+// Author:
+//   @jorgeepunan © 2015
+//   @clsource © 2016
 
-// returns a random item inside an array
+// Picks a random item from an array
 exports.item = function(items) {
   return items[~~(Math.random() * items.length)];
 };
