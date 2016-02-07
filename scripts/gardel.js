75f4c25fa539c6049e75ae9938c4d9f5fed1870e
Héctor Palma Téllez <hector@palmatellez.cl>
2016-02-07 01:50:11 -0300

    wut

--- scripts/gardel.js
+++ scripts/gardel.js
@@ -20,9 +20,10 @@ module.exports = function gardel(robot) {
 
   var today = moment().format('D');
   var lastBusinessDay = moment().endOf('month').isBusinessDay() ? moment().endOf('month').format('D') : moment().endOf('month').prevBusinessDay().format('D');
+  var nameLastDay = moment().locale('es').endOf('month').isBusinessDay() ? moment().locale('es').endOf('month').format('dddd') : moment().locale('es').endOf('month').prevBusinessDay().format('dddd');
   var dayCount = lastBusinessDay - today;
 
   robot.respond(/(gardel)/, function(msg) {
-    return msg.send('Faltan ' + dayCount + ' días para que paguen.');
+    return msg.send('Faltan ' + dayCount + ' días para que paguen.' + ' Este mes pagan el ' + lastBusinessDay + ', que cae ' + nameLastDay + ' :tired_face:');
   });
 };
