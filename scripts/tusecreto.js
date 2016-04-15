2cc1036ad4eff85c1ad97784c8b90aa78fcbf723
Leonardo Gatica <lgatica@protonmail.com>
2016-04-15 14:00:05 -0300

    Fix encoding

--- scripts/tusecreto.js
+++ scripts/tusecreto.js
@@ -1,6 +1,6 @@
 // Description:
-//   Tu secreto queda entre tú y :huemul:
-//   Dile un secreto a @huemul por DM y éste lo anunciará en el canal #random sin mencionarte.
+//   Tu secreto queda entre tÃº y :huemul:
+//   Dile un secreto a @huemul por DM y Ã©ste lo anunciarÃ¡ en el canal #random sin mencionarte.
 //
 // Dependencies:
 //   None
@@ -17,15 +17,15 @@
 module.exports = function(robot) {
 	return robot.respond(/mi secreto (.*)/i, function(msg) {
 		var secreto = msg.match[1];
-		
+
 		var words = ['@here', '@channel', '@group', '@everyone'];
 
 		for (var i = 0; i < words.length; i++) {
 			if (secreto.indexOf(words[i]) !== -1) {
-				return robot.messageRoom('#random', "El tonto de " + msg.message.user.name + " trató de usar @");
+				return robot.messageRoom('#random', "El tonto de " + msg.message.user.name + " tratÃ³ de usar @");
 			}
 		}
-		
+
 		return robot.messageRoom('#random', ":speak_no_evil: *Un secreto:* " + secreto);
 	});
 };
